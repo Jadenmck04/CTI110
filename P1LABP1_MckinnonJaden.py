fccf1cc98e4f5810f36905e15dc7c48512e4351a
Jadenmck04 <jadenmck04@gmail.com>
2026-06-14 15:25:07 -0400

    Add files via upload

---
+++ P1LABP1_MckinnonJaden.py
@@ -0,0 +1,11 @@
+# Jaden McKinnon
+# June 14, 2026
+# P1LAB1 - Welcome Program
+# This program asks the user for their first and last name
+# and displays a welcome message.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print()
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
